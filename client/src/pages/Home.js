959ab5d144a923508b0bedf4d8c1ec973f9194ff
jessicap5159 <jessicaj5159@gmail.com>
2021-06-19 14:09:02 -0700

    created thought form component

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -4,6 +4,7 @@ import Auth from "../utils/auth";
 import { useQuery  } from '@apollo/client'; // allows us to make requests to GraphQL server we connected to
 import ThoughtList from '../components/ThoughtList';
 import { QUERY_THOUGHTS, QUERY_ME_BASIC } from '../utils/queries';
+import ThoughtForm from '../components/ThoughtForm';
 
 const Home = () => {
   // use useQuery hook to make query request
@@ -19,12 +20,13 @@ const Home = () => {
     return (
       <main>
         <div className="flex-row justify-space-between">
+          {loggedIn && (
+            <div className="col-12 mb-3">
+              <ThoughtForm />
+              </div>
+          )}
           <div className={`col-12 mb-3 ${loggedIn && 'col-lg-8'}`}>
-            {loading ? (
-              <div>Loading...</div>
-            ) : (
-              <ThoughtList thoughts={thoughts} title="Some Feed for Thought(s)..." />
-            )}
+            
           </div>
           {loggedIn && userData ? (
             <div className="col-12 col-lg-3 mb-3">
